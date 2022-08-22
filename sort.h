3199edbc66d375bf1c7f68be8f99ecd284e51e42
sheggz <emmanuelikoya99@gmail.com>
2022-08-22 18:54:37 +0000

    [Update] header file to be betty compliant

--- sort.h
+++ sort.h
@@ -20,32 +20,7 @@ typedef struct listint_s
 } listint_t;
 
 
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-	const char *value;
-	const kind_t kind;
-} card_t;
 
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -61,6 +36,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
