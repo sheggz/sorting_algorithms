e269fdbcc44251a11be8e7afe9c9ded0c304293d
sheggz <emmanuelikoya99@gmail.com>
2022-08-22 18:38:01 +0000

    [Update] to avoid wasteful swaps

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -38,7 +38,8 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_recur(int *array, int lo, int hi, size_t size)
 {
-	int i, pivot, hi_idx, lo_idx, temp, lower_than_pivot = lo - 1;
+	size_t i, hi_idx, lo_idx, lower_than_pivot = lo - 1;
+	int pivot, temp;
 	/*
 	 * initialize to non-zero value since
 	 * we aint sure if idx 0 holds a num < than
@@ -60,24 +61,45 @@ void quick_sort_recur(int *array, int lo, int hi, size_t size)
 		{
 			lower_than_pivot++;
 			/*
+			 * if a number < pivot exists at position i=0, then we
+			 * dont have to swap that num with itself. Also if all
+			 * the numbers building up to the number in focus have
+			 * been less than pivot then this number shouldnt be
+			 * swapped if it is < pivot. Else we
 			 * swap element lower than pivot with array[i] so that
 			 * at the end we are sure tha all elements < pivot occupy
 			 * positions 0-lower_than_pivot, then we put the Pivot
 			 * after these
 			 */
-			temp = array[lower_than_pivot];
-			array[lower_than_pivot] = array[i];
-			array[i] = temp;
+			if (i > 0 && array[i - 1] > pivot)
+			{
+				temp = array[lower_than_pivot];
+				array[lower_than_pivot] = array[i];
+				array[i] = temp;
+				/* print the array after every swap */
+				print_array(array, size);
+			}
 		}
 	}
 	/*
 	 * now swap the pivot with array[lower_than_pivot + 1] so that the
 	 * pivot is now in its ideal position
 	 */
-	temp = array[hi_idx];
-	array[hi_idx] = array[lower_than_pivot + 1];
-	array[lower_than_pivot + 1] = temp;
-	print_array(array, size); /* print once pivot is in d right position */
+
+	/* if (lower_than_pivot < hi_idx - 1) */
+	/*
+	 *A number swapping with itself is wasteful. therefore we reduce
+	 * wasteful swaps by checking if numbers that are > than the pivot
+	 * exist before the pivot. if they do, they shoulf be just before the
+	 * pivot since the array has been re-arranged to have all numbers <
+	 * pivot towards the begining of the array.
+	 */
+	if (array[hi_idx - 1] > pivot)
+	{
+		array[hi_idx] = array[lower_than_pivot + 1];
+		array[lower_than_pivot + 1] = pivot;
+		print_array(array, size); /* print once pivot is in d right position */
+	}
 	/* sort left part */
 	quick_sort_recur(array, lo_idx, lower_than_pivot, size);
 	/*sort right part */
--- mains/2-main.c
+++ mains/2-main.c
@@ -9,13 +9,21 @@
  */
 int main(void)
 {
-    int array[] = {19, 73, 48, 99, 19, 71, 13, 52, 96, 19, 73, 86, 7};
+    int array[] = {1,2,3,4,5,6,7,8};
+    /*{19, 73, 48, 99, 19, 71, 13, 52, 96, 19, 73, 86, 7}*/
+    int array2[] = {-92, 3, 2, -31, -76, -54, -52, -88, -7, -74, -6, -53, 4, -9, -78, -15, -83, -3, -23, -96};
     size_t n = sizeof(array) / sizeof(array[0]);
+    size_t n2 = sizeof(array2) / sizeof(array2[0]);
 
     print_array(array, n);
     printf("\n");
     selection_sort(array, n);
     printf("\n");
     print_array(array, n);
+    print_array(array2, n2);
+    printf("\n");
+    selection_sort(array2, n2);
+    printf("\n");
+    print_array(array2, n2);
     return (0);
 }
